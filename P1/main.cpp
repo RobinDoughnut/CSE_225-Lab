55cf908a245adc6e1523b9ae909d4a5765b637eb
RobinDoughnut <taasinbh2000@gmail.com>
2022-10-08 19:52:09 +0600

    Added palindrome checker function

--- P1/main.cpp
+++ P1/main.cpp
@@ -3,12 +3,37 @@
 //  P1
 //
 //  Created by Taasin Bin Hossain Alvi on 10/8/22.
-//
+// Write a function to check palindrome
 
 #include <iostream>
+using namespace std;
+
+
+void PalCheck(int n) {
+    int r, number, sum = 0;
+    bool flag = false;
+    number = n;
+    do
+    {
+        r = n % 10;
+        sum = (sum * 10) + r;
+        n = n / 10;
+        
+        
+    } while (n != 0);
+    if(sum == number) {
+        flag = true;
+    }
+    
+    if(flag == true) {
+        printf("Number is palindrome! \n");
+    } else printf("Not palindrome \n");
+    
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    PalCheck(121);
+    
     return 0;
 }
