7b1a4384271c0e5cb93d45203b440e3607e55b47
RobinDoughnut <taasinbh2000@gmail.com>
2022-10-08 19:56:44 +0600

    Initial Commit

---
+++ P2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  P2
+//
+//  Created by Taasin Bin Hossain Alvi on 10/8/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
